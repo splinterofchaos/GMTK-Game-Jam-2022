6d1aa0a56589c12251f293eb1e833bb52863983c
Scott Prager <splinterofchaos@gmail.com>
2022-07-16 09:11:15 -0500

    Simple title screen.

---
+++ Assets/Intro Screen/ScenePicker.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScenePicker : MonoBehaviour {
+    [SerializeField] Button button;
+    [SerializeField] TMPro.TextMeshProUGUI buttonText;
+
+    [SerializeField] string _sceneName;
+
+    public string sceneName {
+        get => _sceneName;
+        set {
+            _sceneName = value;
+            buttonText.text = value;
+        }
+    }
+
+    public void OnClick() => SceneManager.LoadScene(sceneName);
+
+    private void OnValidate() {
+        sceneName = sceneName;
+    }
+}
