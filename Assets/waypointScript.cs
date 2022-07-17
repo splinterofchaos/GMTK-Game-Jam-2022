d454fd76f583d2857e13a9890a84c9a02e925303
Scott Prager <splinterofchaos@gmail.com>
2022-07-16 19:52:29 -0500

    Make enemy ships properly rotate.

--- Assets/waypointScript.cs
+++ Assets/waypointScript.cs
@@ -19,21 +19,27 @@ public class waypointScript : MonoBehaviour
         baserotation = this.transform.rotation;
         dampDirection = Vector3.up;
         angle = transform.rotation.eulerAngles.z;
-        this.GetComponent<Rigidbody2D>();
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    Vector3 DeltaToWaypoint() {
+        Vector3 delta = nextwaypoint.position - this.transform.position;
+        delta.z = 0;
+        return delta;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Vector3 direction = Vector3.SmoothDamp(dampDirection, (nextwaypoint.position - this.transform.position).normalized, ref turnVelocity, 0.5f) ;
+        Vector3 delta = DeltaToWaypoint();
+        Vector3 direction = Vector3.SmoothDamp(dampDirection, delta.normalized, ref turnVelocity, 0.5f) ;
         dampDirection = direction;
 
-        //var angle = Mathf.Acos(direction.x) * 57.2958f + (direction.y > 0 ? 0 : 180);
+        angle = Mathf.Rad2Deg * Mathf.Atan2(direction.y, direction.x) - 90;
+        rb.MoveRotation(angle);
 
-        //rb.MoveRotation(angle);
-        
-        this.transform.position += direction * speed * Time.deltaTime;
-        if ((this.transform.position - nextwaypoint.position).magnitude < 2)
+        rb.MovePosition(this.transform.position + direction * speed * Time.fixedDeltaTime);
+        if (delta.magnitude < 2)
         {
             nextwaypoint = GameManager.singleton.GetWaypointByIndex(++waypointindex);
             Debug.Log("Reached waypoint " + waypointindex);
